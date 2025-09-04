77d64e8d445eb06cb7bb1cc9ba6cb83d3533fc21
Ali Zaidi <syedalzd772@gmail.com>
2025-09-03 23:01:13 -0600

    Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import  LandingPage  from './pages/LandingPage';
+import AnalyzePage from './pages/AnalyzePage';
+import ResultsPage from './pages/ResultsPage';
+
+function App() {
+
+
+  return (
+    <>
+    <Routes>
+    <Route path = "/" element = { <LandingPage />} />
+    <Route path = "/analyze" element = { <AnalyzePage />} />
+    <Route path = "/results" element = { <ResultsPage />} />
+    </Routes>
+
+    </>
+  )
+}
+
+export default App
